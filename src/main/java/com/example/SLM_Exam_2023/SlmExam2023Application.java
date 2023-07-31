8ed040681be688916f098ea8eebf82defe0e3607
Stefan <wi21b044@technikum-wien.at>
2023-07-31 11:58:07 +0200

    Initial commit

---
+++ src/main/java/com/example/SLM_Exam_2023/SlmExam2023Application.java
@@ -0,0 +1,13 @@
+package com.example.SLM_Exam_2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmExam2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlmExam2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SLM_Exam_2023/SlmExam2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SLM_Exam_2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmExam2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
